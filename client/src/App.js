3cbbd6bea09b6bb383646783e31edf7c39030596
prashant maurya <0166621m@gmail.com>
2022-04-03 23:10:12 +0530

    setting up frontEnd

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </Fragment>
+    </Router>
+  </Provider>
+);
+
+export default App;
