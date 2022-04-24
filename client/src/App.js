131e924e02ee416229e68ebb632cb23dba225bb0
prashant maurya <0166621m@gmail.com>
2022-04-24 19:41:44 +0530

    setup client

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -15,6 +16,7 @@ const App = () => (
     <Router>
       <Fragment>
         <Navbar />
+        <Alert />
         <Routes>
           <Route path='/' element={<Landing />} />
           <Route path='/login' element={<Login />} />
