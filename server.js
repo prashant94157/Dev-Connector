0cb03901787ae87b678bc060e43179e0984ab4eb
Prashant Maurya <62278497+prashant94157@users.noreply.github.com>
2022-11-24 04:03:17 +0530

    commit

--- server.js
+++ server.js
@@ -12,18 +12,18 @@ dotenv.config();
 connectDB();
 
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 //Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-// app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path.join(__dirname, './client/build')));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, './client/build/index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
